609019cb6a5572c31c21cf633ab43581b11043c5
motoo <aofa4566@sina.com>
2026-05-08 18:40:05 +0800

    fix: 移除 yarn-only 限制

--- .electron-vue/preinstall.js
+++ .electron-vue/preinstall.js
@@ -5,8 +5,3 @@ if (nodeMajor < 14) {
   console.error('[ERROR] Node.js v14 or above is required.\n')
   process.exit(1)
 }
-
-if (!/yarn\.js$/.test(process.env.npm_execpath)) {
-  console.error('[ERROR] Please use yarn to install dependencies.\n')
-  process.exit(1)
-}
